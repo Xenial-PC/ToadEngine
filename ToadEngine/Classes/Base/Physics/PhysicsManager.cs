0734e37f62cfc9ca8de2372b545da979d3a532dd
Xenial-PC <58645774+Xenial-PC@users.noreply.github.com>
2025-12-06 19:40:34 -0800

    [REF] ToadEngine: Moved physics simulation to its own class

--- ToadEngine/Classes/Base/Physics/PhysicsManager.cs
+++ ToadEngine/Classes/Base/Physics/PhysicsManager.cs
@@ -1,9 +1,4 @@
-﻿using BepuPhysics;
-using BepuPhysics.CollisionDetection;
-using BepuUtilities;
-using BepuUtilities.Memory;
-
-namespace ToadEngine.Classes.Base.Physics;
+﻿namespace ToadEngine.Classes.Base.Physics;
 
 public class PhysicsManager 
 {
@@ -24,82 +19,4 @@ public class PhysicsManager
     public static List<PhysicsSimulation> GetSimulations => Physics.Select(s => s.Value).ToList();
 
     public static void Reset() => Physics.Clear();
-}
-
-public class PhysicsComponent
-{
-    public BodyHandle BodyHandle;
-    public bool IsDynamic;
-}
-
-public class PhysicsSimulation : IDisposable
-{
-    public bool IsPhysicsPaused, IsSetup;
-    public Simulation Simulation { get; set; } = null!;
-    public BufferPool BufferPool { get; set; } = null!;
-    public ColliderManager ColliderManager { get; set; } = null!;
-
-    public ThreadDispatcher ThreadDispatcher = null!;
-
-    public T NarrowPhaseAs<T>() where T : struct => (T)_narrowPhase;
-    public T PoseIntegratorAs<T>() where T : struct => (T)_poseIntegrator;
-
-    private INarrowPhaseCallbacks _narrowPhase = null!;
-    private IPoseIntegratorCallbacks _poseIntegrator = null!;
-
-    public SolveDescription SolveDescription { get; set; } = new(8, 1);
-
-    public void Setup()
-    {
-        if (IsSetup) return;
-
-        ThreadDispatcher = new ThreadDispatcher(int.Max(1,
-            Environment.ProcessorCount > 4 ? Environment.ProcessorCount - 2 : Environment.ProcessorCount - 1));
-
-        var narrowPhase = new DefaultCallBacks.NarrowPhaseCallbacks();
-        _narrowPhase = narrowPhase;
-
-        var pose = new DefaultCallBacks.PoseIntegratorCallbacks() { Gravity = new System.Numerics.Vector3(0, -10, 0) };
-        _poseIntegrator = pose;
-
-        BufferPool = new BufferPool();
-        Simulation = Simulation.Create(BufferPool, narrowPhase, pose, SolveDescription);
-        ColliderManager = new ColliderManager(Simulation, BufferPool);
-        IsSetup = true;
-    }
-
-    public void Setup<TNarrow, TPose>()
-        where TNarrow : struct, INarrowPhaseCallbacks
-        where TPose : struct, IPoseIntegratorCallbacks
-    {
-        if (IsSetup) return;
-
-        ThreadDispatcher = new ThreadDispatcher(int.Max(1,
-            Environment.ProcessorCount > 4 ? Environment.ProcessorCount - 2 : Environment.ProcessorCount - 1));
-
-        var narrowPhase = new TNarrow();
-        _narrowPhase = narrowPhase;
-
-        var pose = new TPose();
-        _poseIntegrator = pose;
-
-        BufferPool = new BufferPool();
-        Simulation = Simulation.Create(BufferPool, narrowPhase, pose, SolveDescription);
-        ColliderManager = new ColliderManager(Simulation, BufferPool);
-        IsSetup = true;
-    }
-
-    public void Step(float deltaTime)
-    {
-        Simulation.Timestep(deltaTime);
-        TriggerManager.EndFrame();
-    }
-
-    public void Dispose()
-    {
-        TriggerRegistry.Triggers.Clear();
-        ThreadDispatcher.Dispose();
-        Simulation.Dispose();
-        ((IDisposable)BufferPool).Dispose();
-    }
 }
---
+++ ToadEngine/Classes/Base/Physics/PhysicsSimulation.cs
@@ -0,0 +1,84 @@
+﻿using BepuPhysics.CollisionDetection;
+using BepuPhysics;
+using BepuUtilities.Memory;
+using BepuUtilities;
+
+namespace ToadEngine.Classes.Base.Physics;
+
+public class PhysicsComponent
+{
+    public BodyHandle BodyHandle;
+    public bool IsDynamic;
+}
+
+public class PhysicsSimulation : IDisposable
+{
+    public bool IsPhysicsPaused, IsSetup;
+    public Simulation Simulation { get; set; } = null!;
+    public BufferPool BufferPool { get; set; } = null!;
+    public ColliderManager ColliderManager { get; set; } = null!;
+
+    public ThreadDispatcher ThreadDispatcher = null!;
+
+    public T NarrowPhaseAs<T>() where T : struct => (T)_narrowPhase;
+    public T PoseIntegratorAs<T>() where T : struct => (T)_poseIntegrator;
+
+    private INarrowPhaseCallbacks _narrowPhase = null!;
+    private IPoseIntegratorCallbacks _poseIntegrator = null!;
+
+    public SolveDescription SolveDescription { get; set; } = new(8, 1);
+
+    public void Setup()
+    {
+        if (IsSetup) return;
+
+        ThreadDispatcher = new ThreadDispatcher(int.Max(1,
+            Environment.ProcessorCount > 4 ? Environment.ProcessorCount - 2 : Environment.ProcessorCount - 1));
+
+        var narrowPhase = new DefaultCallBacks.NarrowPhaseCallbacks();
+        _narrowPhase = narrowPhase;
+
+        var pose = new DefaultCallBacks.PoseIntegratorCallbacks() { Gravity = new System.Numerics.Vector3(0, -10, 0) };
+        _poseIntegrator = pose;
+
+        BufferPool = new BufferPool();
+        Simulation = Simulation.Create(BufferPool, narrowPhase, pose, SolveDescription);
+        ColliderManager = new ColliderManager(Simulation, BufferPool);
+        IsSetup = true;
+    }
+
+    public void Setup<TNarrow, TPose>()
+        where TNarrow : struct, INarrowPhaseCallbacks
+        where TPose : struct, IPoseIntegratorCallbacks
+    {
+        if (IsSetup) return;
+
+        ThreadDispatcher = new ThreadDispatcher(int.Max(1,
+            Environment.ProcessorCount > 4 ? Environment.ProcessorCount - 2 : Environment.ProcessorCount - 1));
+
+        var narrowPhase = new TNarrow();
+        _narrowPhase = narrowPhase;
+
+        var pose = new TPose();
+        _poseIntegrator = pose;
+
+        BufferPool = new BufferPool();
+        Simulation = Simulation.Create(BufferPool, narrowPhase, pose, SolveDescription);
+        ColliderManager = new ColliderManager(Simulation, BufferPool);
+        IsSetup = true;
+    }
+
+    public void Step(float deltaTime)
+    {
+        Simulation.Timestep(deltaTime);
+        TriggerManager.EndFrame();
+    }
+
+    public void Dispose()
+    {
+        TriggerRegistry.Triggers.Clear();
+        ThreadDispatcher.Dispose();
+        Simulation.Dispose();
+        ((IDisposable)BufferPool).Dispose();
+    }
+}
